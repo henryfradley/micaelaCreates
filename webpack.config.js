29da1df1554498848b49ee320d4d8fa6cb49d32f
Henry Fradley <fradleyhenry@gmail.com>
2021-01-28 17:45:00 -1000

    production Ready

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './client/src/App.jsx',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './client/public')
